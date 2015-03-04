bbf1d925bfffbce2084f1357ab72fef2045246f5
Acid Icosahedron <icosacid@gmail.com>
2015-03-04 15:44:58 +0100

    Drag&drop anywhere
    Drag&drop works on SVG elements, next step the drop area should be
    restricted to the ring only.

--- js/brainuke.js
+++ js/brainuke.js
@@ -12,10 +12,12 @@ BUKE.shapeCentral = function(){
 	jQuery('#central svg').append("<circle class='outerSmall'/>");
 	jQuery('.outerBig').attr('cx',300).attr('cy',200)
 		.attr('r',this.rIn).attr('fill','none')
-		.attr('stroke-width',3).attr('stroke','black');
+		.attr('stroke-width',3).attr('stroke','black')
+		.css('z-index',0);
 	jQuery('.outerSmall').attr('cx',300).attr('cy',200)
 		.attr('r',this.rOut).attr('fill','none')
-		.attr('stroke-width',3).attr('stroke','black');
+		.attr('stroke-width',3).attr('stroke','black')
+		.css('z-index',0);
 	for (var i=1;i<=this.circles;i++){
 		jQuery('#central svg').append("<circle class='ball' id='ball"+i+"' />");
 		// Loop circle values
@@ -25,7 +27,8 @@ BUKE.shapeCentral = function(){
 		var cy = 200-rFromCenter * Math.sin(i/this.circles*2*Math.PI);
 		jQuery('#ball'+i).attr('cx',cx).attr('cy',cy)
 		.attr('r',rBall).attr('fill','#7ae')
-		.attr('stroke-width',1).attr('stroke','black');
+		.attr('stroke-width',1).attr('stroke','black')
+		.css('z-index',0);
 	}
 	
 }
@@ -37,6 +40,10 @@ jQuery(document).ready(function(){
 	// Trick to make the svg elements visible
 	setTimeout(function(){
 		jQuery('#central').html(jQuery('#central').html());
+		for (var i=1;i<=BUKE.circles;i++){
+			dragMe('#ball'+i,'#central svg');
+		}
 	},500);
+	
 	
 });
