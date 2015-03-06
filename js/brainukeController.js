bdac81d23d0f60872d2f6023228a32ecebf1b4c1
Acid Icosahedron <icosacid@gmail.com>
2015-03-06 09:36:11 +0100

    Tiny cleaning
    Some not too necessary formal cleaning
    Randomize function for the model
    A bit of CSS

--- js/brainukeController.js
+++ js/brainukeController.js
@@ -1,59 +1,53 @@
 window.app.controller("BrainukeController", ["$scope", "$timeout", "Model", function($scope, $timeout, Model){
 
-	console.log("controle added");
+	console.log("Controller added");
 
-		//get the list of added notes from the model
-		$scope.model=Model; 
-		this.currentNote;
-		this.currentPage=1;
+	//get the list of added notes from the model
+	$scope.model = Model; 
+	this.currentNote;
+	this.currentPage = 1;
 
-		//get the list of players from the model
-		this.players=Model.players;
+	//get the list of players from the model
+	this.players = Model.players;
 
+	//The time is binded with the html, pass value to the model to handle it	
+	this.timeCount;
+	this.gameOn = false;
 
-		//The time is binded with the html, pass value to the model to handle it	
-		this.timeCount;
-		this.gameOn=false;
+	var totalNotes = 0;
 
-		var totalNotes=0;
+	//Current player, it consist of a string of it's name, 
+	//however for future implementation it could be an object that consists of all information of the player
+	this.currentPlayer;
 
-		
-		//Current player, it consist of a string of it's name, 
-		//however for future implementation it could be an object that consists of all information of the player
-		this.currentPlayer;
+	this.addNote = function(note){
+		//add note on the array on the model with all notes
+		Model.addNote(note);
+		totalNotes++;
+	};
 
-		this.addNote=function(note){
-			//add note on the array on the model with all notes
-			Model.addNote(note);
-			totalNotes++;
-		};
+	this.randomize = function(){
+		//ATTENTION:Call the model and randomise the sequence stored
+		Model.randomize();
+	};
 
-		this.randomize=function(){
-			//ATTENTION:Call the model and randomise the sequence stored
-			Model.randomize();
-		};
+	this.startGame = function(){
+		////ATTENTION:Call the model and start the game
+		this.gameOn = true;
+		notesChecked = 0;
+	}
 
+	//These functions are related to the navigation within the the html, I don't now
+	//if it should be in a separated controller
+	this.isSetPage = function(page){
+		return (this.currentPage === page);
+	}
 
-		this.startGame= function(){
-			////ATTENTION:Call the model and start the game
-			this.gameOn=true;
-			notesChecked=0;
-		}
+	//homePage = 1
+	//mainPage = 2
+	//scorePage = 3
+	this.setPage = function(pageId){
+		this.currentPage = pageId;
+	}
 
-
-		//This function are related to the navigation within the the html, I don't now if it would 
-		//if it should be in a separated controller
-		this.isSetPage=function(page){
-			return this.currentPage===page;
-		}
-
-		//homePage= 1
-		//mainPage= 2
-		//scorePage= 3
-		this.setPage=function(pageId){
-			this.currentPage=pageId;
-		}
-
-
-
-	}]);
+}]);
--- js/model.js
+++ js/model.js
@@ -1,85 +1,109 @@
 window.app.service("Model", function(){
 
-	this.notes=[];
-	this.score=0;
-	this.players=[];
+	this.notes = [];
+	this.score = 0;
+	this.players = [];
 
 ///////////////TEST AREA - REMOVE AFTERWARDS///////////
 
-
 	//Testing score board
-	maria=new Object();
-	maria.name="augusto";
-	maria.score=333;
+	maria = new Object();
+	maria.name = "augusto";
+	maria.score = 333;
 
-	maria2=new Object();
-	maria2.name="maria";
-	maria2.score=24523;
+	maria2 = new Object();
+	maria2.name = "maria";
+	maria2.score = 24523;
 
-	maria3=new Object();
-	maria3.name="mariama";
-	maria3.score=333333;
+	maria3 = new Object();
+	maria3.name = "mariama";
+	maria3.score = 333333;
 
-	maria4=new Object();
-	maria4.name="sheng";
-	maria4.score=33003;
+	maria4 = new Object();
+	maria4.name = "sheng";
+	maria4.score = 33003;
 
 	this.players.push(maria);
 	this.players.push(maria2);
 	this.players.push(maria3);
 	this.players.push(maria4);
+	
  ///////////////////////////////////////////////////////
 
-
-
-
-//Add player on the list of players (name, score and rank)
-this.addPlayer= function(player){
-	this.players.push(player);
-}
-
-
-//Add note on the array of notes and update its position os the viewPort
-this.addNote=function(note){
-
-	var newNote = new Object();
-	newNote.name=note;
-
-	//This variables are used to define the style
-	newNote.verified=false; //set as not verified initially
-	newNote.isRight=true; //set the note as right when it is added
-
-	//ATTENTION: The oldest note is in the end of the array
-	this.notes.unshift(newNote);
-	updateShapesPosition.call(this);
-};
-
-//todo: function that randomise the order of the note on the array
-this.randomize= function(){
-
-}
-
-
-//todo: function triggered to check a note in the array
-this.checkNote= function(position){
-
-	
-}
-
-
-
-//It calculates the cordinates of the shapes on the viewPort
-//and updates the their coordinates
-var updateShapesPosition= function(){
-
-	var numberNotes=this.notes.length;
-
-	for (var i=1;i<=numberNotes;i++){
-		// Loop coordinate shape values 
-		this.notes[i-1].x = 300+230 * Math.cos(i/numberNotes*2*Math.PI+(Math.PI/2));
-		this.notes[i-1].y= 300-230 * Math.sin(i/numberNotes*2*Math.PI+(Math.PI/2));
+	//Add player on the list of players (name, score and rank)
+	this.addPlayer = function(player){
+		this.players.push(player);
 	}
-};
 
+
+	//Add note on the array of notes and update its position on the viewPort
+	this.addNote = function(note){
+
+		var newNote = new Object();
+		newNote.name = note;
+
+		//These variables are used to define the style
+		newNote.verified = false; //set as not verified initially
+		newNote.isRight = true; //set the note as right when it is added
+
+		//ATTENTION: The oldest note is in the end of the array
+		this.notes.unshift(newNote);
+		updateShapesPosition.call(this);
+	};
+
+	// Function that randomises the order of the notes in the array
+	this.randomize = function(){
+		var order = this.randomIntIndexList(this.notes.length,0,this.notes.length-1);
+		var notesCopy = this.notes.slice();
+		for (key in this.notes){
+			this.notes[key] = notesCopy[order[key]];
+		}
+	}
+	
+	// Function that returns an Int array with random values
+	this.randomIntIndexList = function(size,min,max){
+		// size must be > (max-min)
+		if(size > (max-min+1)){
+			console.log("Values not accepted in randomIntIndexList()");
+			return;
+		}
+		// min & max included
+		var i=0;
+		var randomNum;
+		var isAlreadyThere = false;
+		var list = [];
+		while(i<size){
+			// Generate random number
+			randomNum = Math.floor(Math.random()*(max-min+1)+min);
+			// If already in list
+			isAlreadyThere = false;
+			for (var t=0;t<list.length;t++){
+				if(randomNum == list[t]){isAlreadyThere = true}
+				else{};
+			}
+			if(isAlreadyThere){}
+			else{i++;list.push(randomNum);}
+		}
+		return list;
+	}
+
+
+	//todo: function triggered to check a note in the array
+	this.checkNote = function(position){
+
+	}
+
+	//It calculates the cordinates of the shapes on the viewPort
+	//and updates the their coordinates
+	var updateShapesPosition = function(){
+
+		var numberNotes = this.notes.length;
+
+		for (var i=1;i<=numberNotes;i++){
+			// Loop coordinate shape values 
+			this.notes[i-1].x = 300+230 * Math.cos(i/numberNotes*2*Math.PI+(Math.PI/2));
+			this.notes[i-1].y= 300-230 * Math.sin(i/numberNotes*2*Math.PI+(Math.PI/2));
+		}
+	};
 
 });
