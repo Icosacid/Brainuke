5565630236bdf0cab523cebd50e2db87e02ee7fb
Acid Icosahedron <icosacid@gmail.com>
2015-03-11 09:36:54 +0100

    Small improvements

--- README.md
+++ README.md
@@ -5,29 +5,35 @@ by Midas Nouwens, Mariama Oliveira, Seng Li & Alexandre Andrieux
 
 ## Navigation
 * Save player's name in variable which will be used in score screen
+* Reset player's name input when navigating back to start page
 
 ## Game session
+
 ### UI
 * Add SVG text for the note detected by the microphone (under the requested note?)
 * Add remove note function
 * Add buttons/list for predefined sequences
 * Add progression/XP/score bar
 * Add feedback animation when note is performed with success
+
 ### Preparation phase
-* Add preparation phase boolean variable
 * Allow drag & drop, removal and adding only in preparation phase
 * Swap positions as well when randomizing, not just the order in the players array
 * Implement drag&drop
+
 ### Sound
-* (All done?)
+* (all done?)
+
 ### Notes sequences
 * Add predefined sequences (array of notes)
 
 ## Design
 * Add background for game screen
 * Smooth transition between screens (find some trick, since angluar ng-show/ng-hide directives force the CSS property display:none)
-
+* Design for SVG elements
 
 # Discussion
 * Shall we do the two modes, practice VS live stream? (like step-by-step VS guitar hero)
 * Shall we include time in practice mode to get more XP/score in the bar?
+* How can we include this time input we have (under the Random button) in the game? Live stream mode?
+* Is it better if we re-ask for permission to use microphone for each game, or only once?
--- js/brainukeController.js
+++ js/brainukeController.js
@@ -1,7 +1,5 @@
 window.app.controller("BrainukeController", ["$scope","$interval", "$timeout", "Model", "Audiuke", function($scope, $interval, $timeout, Model, Audiuke) {
 
-	console.log("Controller added");
-	
 	// Model service
 	$scope.model = Model;
 	$scope.players = Model.players;
@@ -14,92 +12,101 @@ window.app.controller("BrainukeController", ["$scope","$interval", "$timeout", "
 	$scope.currentPage = 1;
 	
 	// Game variables
-	//Current player, it consist of a string of it's name, 
-	//however for future implementation it could be an object that consists of all information of the player
+	// Current player, it consist of a string of it's name, 
+	// however for future implementation it could be an object that consists of all information of the player
 	$scope.currentPlayer;
 	$scope.currentNote;
-	$scope.gameOver = false;
+	
+	$scope.gamePrep = false;
 	$scope.gameOn = false;
+	$scope.mikeOn = false;
+	$scope.gameOver = false;
+	
 	$scope.intervalPromise;
 	//The time is binded with the html, pass value to the model to handle it
 	$scope.timeCount;
 	$scope.totalNotes = 0;
 	$scope.gameStep = 1;
-	
-	$scope.mikeOn = false;
 
 	/** View functions **/
-	
 	$scope.isSetPage = function(page) {
 		return ($scope.currentPage === page);
 	}
-	//homePage = 1, mainPage = 2, scorePage = 3
 	$scope.setPage = function(pageId) {
+		//homePage = 1, mainPage = 2, scorePage = 3
 		$scope.currentPage = pageId;
+		if (pageId == 2) {
+			$scope.gamePrep = true;
+		}
 	}
 	
 	/** Game setup functions **/
 	$scope.addNote = function(note) {
-		//add note on the array on the model with all notes
 		$scope.model.addNote(note);
 		$scope.totalNotes++;
 	};
 	$scope.randomize = function() {
-		//ATTENTION:Call the model and randomise the sequence stored
 		$scope.model.randomize();
 	};
 
 	/** Game functions **/
 	$scope.startGame = function() {
+		$scope.gamePrep = false;
 		$scope.gameOn = true;
+		// Start the loop
 		$scope.intervalPromise = $interval(function() {
 			if ($scope.mikeOn) {
 				$scope.gameLoop();
 			}
 			// Test note
-		}, 200);
+		}, 150);
+		// Start microphone (and success callback)
 		$scope.audiuke.init(function(stream) {
 			$scope.mikeOn = true;
 			$scope.audiuke.gotStream(stream);
+			// Load first note
 			$scope.currentNote = $scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].name;
 			console.log("New note:" + $scope.currentNote);
 		});
 		
 	}
-	// Takes the game to the next step (test new note)
 	$scope.nextStep = function() {
-		
+		// Success
+		console.log('Success!');
+		// View feedback that note is OK
+		$scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].verified = true;
+		$scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].isRight = true;
+		// Next step
+		$scope.gameStep++;
+		// Might be over!
+		if (($scope.gameStep-1) !== $scope.totalNotes) {
+			// Next step indeed
+			$scope.currentNote = $scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].name;
+			console.log("New note:" + $scope.currentNote);
+		} else {
+			// Game over!
+			$scope.gameOver();
+		}
 	}
-	
+	$scope.gameOver = function() {
+		// Killing the gameLoop
+		$interval.cancel($scope.intervalPromise);
+		
+		// Showing the score page and reseting the game after a little while
+		$timeout(function() {
+			$scope.resetGame();
+			$scope.setPage(3);
+		}, 1000);
+	}
 	$scope.gameLoop = function() {
-		console.log('You play:' + $scope.audiuke.noteString);
+		console.log('You are playing:' + $scope.audiuke.noteString);
 		if ($scope.audiuke.noteString == $scope.currentNote) {
-			// Success
-			console.log('Success!');
-			// View feedback that note is OK
-			$scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].verified = true;
-			$scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].isRight = true;
-			// Next step
-			$scope.gameStep++;
-			// Might be over!
-			if (($scope.gameStep-1) !== $scope.totalNotes) {
-				// Next step indeed
-				$scope.currentNote = $scope.model.notes[$scope.totalNotes-1-($scope.gameStep-1)].name;
-				console.log("New note:" + $scope.currentNote);
-			} else {
-				// Game over!
-				// Killing the gameLoop
-				$interval.cancel($scope.intervalPromise);
-				// Reseting the game and showing the score page
-				$scope.resetGame();
-				$scope.setPage(3);
-			}
+			$scope.nextStep();
 		} else {
 			// Wait...
 		}
 	}
-
-	$scope.resetGame = function(){
+	$scope.resetGame = function() {
 		$scope.gameOn = false;
 		$scope.mikeOn = false;
 		$scope.totalNotes = 0;
